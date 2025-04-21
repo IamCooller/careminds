2d3c3cc30f4b1e87b20d5f37bcc2413ac62e2522
IamCooller <cooller2000@inbox.ru>
2025-04-21 13:36:34 -0600

    init

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export interface Asset {
+	type: "stock" | "crypto";
+	symbol: string;
+	name: string;
+	quantity: number;
+	purchasePrice: number;
+	currentPrice: number;
+}
+
+export interface Wallet {
+	walletName: string;
+	currentAmount: number;
+	spentAmount: number;
+	profitLoss: number;
+	assets: Asset[];
+}
